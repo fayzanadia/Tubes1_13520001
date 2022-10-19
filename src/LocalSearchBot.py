e88a51b2fdead344be74c6b551f849d4d8c0be76
roastland <tfpruslanali@gmail.com>
2022-10-19 07:45:29 +0700

    add localsearch neighbours

--- src/LocalSearchBot.py
+++ src/LocalSearchBot.py
@@ -4,9 +4,7 @@ from GameState import GameState
 import random
 import numpy as np
 
-class LocalSearchBot(Bot):
-    
-    
+class LocalSearchBot(Bot): 
     
     def get_action(self, state: GameState) -> GameAction:
         all_row_marked = np.all(state.row_status == 1)
@@ -30,7 +28,6 @@ class LocalSearchBot(Bot):
             return self.get_random_col_action(state)
         else:
             return self.get_random_row_action(state)
-        
 
     def get_random_row_action(self, state: GameState) -> GameAction:
         position = self.get_random_position_with_zero_value(state.row_status)
@@ -53,3 +50,36 @@ class LocalSearchBot(Bot):
             valid = matrix[y, x] == 0
         
         return (x, y)
+ 
+    
+    def get_row_neighbours(self, state: GameState):
+        #all_row_not_marked = np.all(state.row_status == 0)     
+        row_neighbours = []     # nanti isinya list of tuples koordinat row yg bisa dipilih
+ 
+        [ny, nx] = state.row_status.shape
+        
+        for i in range(ny):       
+            for j in range(nx):
+                if (state.row_status[i,j] == 0):
+                    neighbour = state.row_status[i,j]
+                    row_neighbours.append(neighbour)
+                    
+        return row_neighbours
+    
+    
+    def get_col_neighbours(self, state: GameState):
+        #all_col_not_marked = np.all(state.col_status == 0)     
+        col_neighbours = []     # nanti isinya list of tuples koordinat col yg bisa dipilih
+ 
+        [ny, nx] = state.col_status.shape
+        
+        for i in range(ny):       
+            for j in range(nx):
+                if (state.col_status[i,j] == 0):
+                    neighbour = state.col_status[i,j]
+                    col_neighbours.append(neighbour)
+                    
+        return col_neighbours
+    
+    def local_search(self, state: GameState):
+        return
